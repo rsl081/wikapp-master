9323244b3b642743b093e9cb8398637c1848abd5
rsl081 <53944109+rsl081@users.noreply.github.com>
2021-11-17 01:07:18 +0800

    rsl/kinder_kulay_1stkwarter

---
+++ Wikapp/Assets/Scripts/MainMenu/Game/Grade1/Tracing.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class Tracing : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler
+{
+    public Image traceImge;
+    float amountToAdd = 0.02f;
+    [SerializeField] int howFar = 6;
+    [SerializeField] private float dampingSpeed = 0.05f; //The closer to zero the faster it goes
+    private RectTransform draggingObject;
+    private Vector3 velocity = Vector3.zero;
+    bool isEndDragEnd = true;
+
+    private void Start() {
+        traceImge = GetComponent<Image>();
+        traceImge.fillAmount = 0f;
+        draggingObject = GetComponent<RectTransform>();
+
+    }
+
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        if(traceImge.fillAmount != 1){
+            isEndDragEnd = true;
+            if(RectTransformUtility.ScreenPointToWorldPointInRectangle(draggingObject, 
+                eventData.position, eventData.pressEventCamera, out var globalMousePosition))
+            {
+                    Vector2 touchPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                    if(globalMousePosition.x == touchPos.x
+                    && Vector2.Distance(draggingObject.transform.position, touchPos) < howFar){
+                        traceImge.fillAmount += amountToAdd;
+                    }
+            }
+        }
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        //traceIamge.fillAmount += amountToAdd;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+   
+        if(traceImge.fillAmount == 1 && isEndDragEnd)
+        {
+            FindObjectOfType<QuizNumberTracing>().numberOfCorrectTrace++;
+
+            if(FindObjectOfType<QuizNumberTracing>().numberOfCorrectTrace ==
+                FindObjectOfType<QuizNumberTracing>().lenOfTracing-1){
+
+                FindObjectOfType<QuizNumberTracing>().OnAnswerSelected();
+                
+            }
+            isEndDragEnd = false;
+        }
+        
+        //traceIamge.fillAmount += amountToAdd;
+    }
+}
---
+++ Wikapp/Assets/Scripts/MainMenu/Game/Grade1/TracingNumberSO.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu (menuName = "QuesitonTracingNumber", fileName ="Question 1")]
+public class TracingNumberSO : ScriptableObject
+{
+    [SerializeField] GameObject question;
+
+    public GameObject GetQuestion()
+    {
+        return question;
+    }
+    // public bool GetCorrectAnswerBool()
+    // {
+    //     return correctAnswer;
+    // }
+    // public string GetAnswer(int index)
+    // {
+    //     return answer[index];
+    // }
+    // public void SetAnswer(int index, string answer)
+    // {
+    //     this.answer[index] = answer;
+    // }
+}
