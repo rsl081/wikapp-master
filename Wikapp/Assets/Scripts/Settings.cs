28f6242f74663e63b6fb1ccd410d7e6e1c3ff9ce
rsl081 <53944109+rsl081@users.noreply.github.com>
2021-12-07 11:01:20 +0800

    rsl-setting-music

---
+++ Wikapp/Assets/Scripts/Settings.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Settings : MonoBehaviour
+{
+    Toggle m_Toggle;
+    [SerializeField] AudioSource[] audioSource;
+    private void Awake() {
+        //PlayerPrefs.DeleteAll();
+        m_Toggle = GetComponentInChildren<Toggle>();
+       // GameObject music = GameObject.FindGameObjectWithTag("Music");
+        
+
+        audioSource = FindObjectsOfType<AudioSource>();
+       
+        if(PlayerPrefs.GetInt("muted") == 1)
+        {
+            foreach(AudioSource audio in audioSource)
+            {
+                audio.Stop();
+            }
+        
+            if(m_Toggle != null)
+            {
+
+                m_Toggle.isOn = false;
+            }
+   
+        }
+    }
+
+
+    void Start()
+    {
+        if(m_Toggle != null)
+        {
+
+            m_Toggle.onValueChanged.AddListener(delegate {
+                ToggleValueChanged(m_Toggle);
+            });
+        
+        }
+    }
+
+    //Output the new state of the Toggle into Text
+    void ToggleValueChanged(Toggle change)
+    {
+        if(m_Toggle.isOn)
+        {
+        
+            foreach(AudioSource audio in audioSource)
+            {
+                audio.Play();
+            }
+            PlayerPrefs.SetInt("muted", 0);
+
+        }else{
+            foreach(AudioSource audio in audioSource)
+            {
+                audio.Stop();
+            }
+            PlayerPrefs.SetInt("muted", 1);
+
+        }
+    }
+}
